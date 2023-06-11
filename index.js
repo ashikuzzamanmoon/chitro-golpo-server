4abb479391cefb759d725d924f17f77e56ebca1d
Ashikuzzaman_Moon <ashikuzzamanmoon31@gmail.com>
2023-06-12 00:04:04 +0600

    create instructor api

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const jwt=require("jsonwebtoken")
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -8,8 +9,28 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// verifyJwt middlewere
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const verifyJwt=(req,res,next)=>{
+  const authorization=req.headers.authorization;
+  if(!authorization){
+    return res.status(401).send({error:true,message:"unauthorize access"})
+  }
+  // bearer token
+  const token=authorization.split(" ")[1];
+  jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err,decoded)=>{
+    if(err){
+      return res
+      .status(401)
+      .send({error:true,message:"unauthorize access"})
+    }
+    req.decoded=decoded;
+    next();
+  })
+}
+
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zjtbp3d.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -36,6 +57,15 @@ async function run() {
       res.send(result);
     })
 
+    // make jwt token
+    app.post("/jwt",(req,res)=>{
+      const user=req.body;
+      const token=jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{
+        expiresIn:'2h',
+      });
+      res.send({token})
+    })
+
 
     // classes api
     app.get('/classes', async (req, res) => {
@@ -55,6 +85,57 @@ async function run() {
       res.send(result);
     })
 
+    
+    app.get('/users', async(req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    })
+
+
+    app.patch('/users/admin/:id', async(req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          role: 'admin'
+        }
+      }
+      const result = await userCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    })
+
+    // admin
+    app.get('/users/admin/:email', async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email }
+      const user = await userCollection.findOne(query);
+      const result = { admin: user?.role === 'admin' }
+      res.send(result);
+    })
+
+    app.patch('/users/admin/:id', async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: 'admin'
+        },
+      };
+      const result = await userCollection.updateOne(filter, updateDoc);
+      res.send(result);
+
+    })
+
+
+    // instructor
+    app.get('/users/instructor/:email', async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email }
+      const user = await userCollection.findOne(query);
+      const result = { admin: user?.role === 'instructor' }
+      res.send(result);
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
